24576335d1eb8ca3c596a1bca9bbd455fe331103
hannupi <varahahmo1@gmail.com>
2024-10-27 13:16:47 +0200

    use dotenv instead of linux env

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+use dotenv::dotenv;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
@@ -19,6 +20,7 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let token = env::var("DISCORD_TOKEN").expect("Missing DISCORD_TOKEN");
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
@@ -26,6 +28,8 @@ async fn main() {
         .event_handler(Handler)
         .await
         .expect("Err creating client");
+
+    println!("bot online");
 
     if let Err(why) = client.start().await {
         println!("Client error: {why:?}");
