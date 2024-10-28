be2e376b0391980fb554829aa38f4ea1a1f59043
hannupi <varahahmo1@gmail.com>
2024-10-28 18:45:43 +0200

    refactor to separate scraper and entry logic

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 
-use apurri_scraper::Scraper;
+mod scraper;
 
-fn main() -> () {
-    let args = env::args().skip(1).collect::<Vec<String>>();
-    if args.is_empty() {
+fn main() {
+    let keywords = env::args().skip(1).collect::<Vec<String>>();
+    if keywords.is_empty() {
         eprint!("Give at least one keyword as args");
         std::process::exit(1);
     }
 
-    let scraper = Scraper::new(args);
+    let scraper = scraper::Scraper::new(keywords);
 
     for keyword in scraper.keywords.iter() {
         scraper.scrape_page(keyword);
---
+++ scraper/src/scraper/mod.rs
@@ -0,0 +1,43 @@
+use reqwest::blocking::Client;
+
+use scraper::{Html, Selector};
+pub mod scrapedentry;
+
+pub struct Scraper {
+    client: Client,
+    pub keywords: Vec<String>,
+}
+
+impl Scraper {
+    pub fn new(keywords: Vec<String>) -> Scraper {
+        Scraper {
+            client: Client::new(),
+            keywords,
+        }
+    }
+
+    pub fn scrape_page(&self, keyword: &str) {
+        let url = format!(
+            "https://www.tori.fi/recommerce/forsale/search?computeracc_type=3&product_category=2.93.3215.46&q={}&sort=PUBLISHED_DESC",
+            keyword
+        );
+        println!("\nSearching keyword '{}' URL: {}", keyword, url);
+        let res = self.client.get(url).send().expect("GET failed");
+        let body = res.text().expect("response -> text failed");
+        let doc = Html::parse_document(&body);
+        let entry_selector =
+            Selector::parse("section > div > article").expect("Failed to create entry selector");
+
+        for item in doc.select(&entry_selector) {
+            match scrapedentry::extract_entry(&item) {
+                Ok(entry) => {
+                    println!(
+                        "Name: {} \nPrice: {}\nImg: {}\n",
+                        entry.name, entry.price, entry.img
+                    );
+                }
+                Err(err) => eprint!("Failed to extract entry: {}", err),
+            }
+        }
+    }
+}
