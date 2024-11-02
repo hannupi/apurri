a9ca68f0331d8dcee659f827bb40436be6df2889
hannu <varahahmo1@gmail.com>
2024-11-02 16:17:11 +0200

    refactor selector logic

--- scraper/src/scraper/scrapedentry.rs
+++ scraper/src/scraper/scrapedentry.rs
@@ -8,15 +8,10 @@ pub struct ScrapedEntry {
 }
 
 pub fn extract_entry(item: &ElementRef) -> Result<ScrapedEntry, String> {
-    let title_selector = create_selector("h2");
-    let img_selector = create_selector("img");
-    let price_selector = create_selector("div > div > span");
-    let url_selector = create_selector("div > h2 > a");
-
-    let title_element = select_element(item, &title_selector).unwrap();
-    let img_element = select_element(item, &img_selector).unwrap();
-    let price_element = select_element(item, &price_selector).unwrap();
-    let url_element = select_element(item, &url_selector).unwrap();
+    let title_element = select_element(item, "h2")?;
+    let img_element = select_element(item, "img")?;
+    let price_element = select_element(item, "div > div > span")?;
+    let url_element = select_element(item, "div > h2 > a")?;
 
     let scraped_entry = ScrapedEntry {
         name: title_element.text().collect(),
@@ -35,15 +30,9 @@ pub fn extract_entry(item: &ElementRef) -> Result<ScrapedEntry, String> {
     Ok(scraped_entry)
 }
 
-fn create_selector(selector: &str) -> Selector {
-    Selector::parse(selector).unwrap()
-}
-
-fn select_element<'a>(
-    item: &ElementRef<'a>,
-    selector: &Selector,
-) -> Result<ElementRef<'a>, String> {
-    item.select(selector)
+fn select_element<'a>(item: &ElementRef<'a>, selector_str: &str) -> Result<ElementRef<'a>, String> {
+    let selector = Selector::parse(selector_str).unwrap();
+    item.select(&selector)
         .next()
         .ok_or_else(|| format!("Error with selector {:?}", selector))
 }
